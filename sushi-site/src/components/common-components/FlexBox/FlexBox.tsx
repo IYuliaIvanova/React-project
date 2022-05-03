8b275b64aabb6ad9b6349ff2cb32994b7d696b0f
Vladseme2012 <vlad.seme2012@yandex.ru>
2022-05-03 22:04:51 +0300

    added padding-left, max-width and background-color properties to UnorderedList component

--- sushi-site/src/components/common-components/FlexBox/FlexBox.tsx
+++ sushi-site/src/components/common-components/FlexBox/FlexBox.tsx
@@ -29,32 +29,32 @@ interface IFlexBoxProps {
   overflow?: string;
 }
 
- export const FlexBox = styled.div<IFlexBoxProps>`
-    width: ${p => typeof p.width === "number" ? p.width+"%" : p.width+"px"};
-    max-width: ${p => p.maxWidth}px;
-    min-width: ${p => p.minWidth}px;
-    height: ${p => p.height}px;
-    max-height: ${p => p.maxHeight}px;
-    display: flex;
-    align-items: ${p => p.alignItems || 'center'};
-    align-self: ${p => p.alignSelf || 'center'};
-   justify-content: ${p => p.justifyContent || 'center'};
-    flex-direction: ${p => p.flexDirection || 'row'};
-    flex-wrap: ${p => p.flexWrap};
-    column-gap: ${p => p.columnGap}px;
-    margin: ${p => p.margin};
-    padding: ${p => p.padding};
-    background-color: ${p => p.bgColor};
-    border-top: ${p => p.borderTop};
-    border-bottom: ${p => p.borderBottom};
-    border-left: ${p => p.borderLeft};
-    border-right: ${p => p.borderRight};
-    position: ${p => p.position};
-    top: ${p => p.top}px;
-    bottom: ${p => p.bottom}px;
-    left: ${p => p.left}px;
-    right: ${p => p.right}px;
-    overflow: ${p => p.overflow};
-    transition: all 0.5s linear;
-    background-color: ${p => p.backColor};
- `
+export const FlexBox = styled.div<IFlexBoxProps>`
+  width: ${p => typeof p.width === "number" ? p.width+"%" : p.width+"px"};
+  max-width: ${p => p.maxWidth}px;
+  min-width: ${p => p.minWidth}px;
+  height: ${p => p.height}px;
+  max-height: ${p => p.maxHeight}px;
+  display: flex;
+  align-items: ${p => p.alignItems || 'center'};
+  align-self: ${p => p.alignSelf || 'center'};
+  justify-content: ${p => p.justifyContent || 'center'};
+  flex-direction: ${p => p.flexDirection || 'row'};
+  flex-wrap: ${p => p.flexWrap};
+  column-gap: ${p => p.columnGap}px;
+  margin: ${p => p.margin};
+  padding: ${p => p.padding};
+  background-color: ${p => p.bgColor};
+  border-top: ${p => p.borderTop};
+  border-bottom: ${p => p.borderBottom};
+  border-left: ${p => p.borderLeft};
+  border-right: ${p => p.borderRight};
+  position: ${p => p.position};
+  top: ${p => p.top}px;
+  bottom: ${p => p.bottom}px;
+  left: ${p => p.left}px;
+  right: ${p => p.right}px;
+  overflow: ${p => p.overflow};
+  transition: all 0.5s linear;
+  background-color: ${p => p.backColor};
+`
--- sushi-site/src/components/common-components/UnorderedList/Ul.tsx
+++ sushi-site/src/components/common-components/UnorderedList/Ul.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import styled, { css } from "styled-components";
 
-export const UnorderedList = styled.ul`
+interface IUnorderedList {
+  maxWidth?: number;
+  backgroundColor?: string;
+}
+
+export const UnorderedList = styled.ul<IUnorderedList>`
   list-style-type: none;
+  padding-left: 0;
+  max-width: ${(p) => p.maxWidth}px;
+  background-color: ${(p) => p.backgroundColor};
 `
+

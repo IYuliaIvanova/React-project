d1b5f9f998b29375a0ab7d9996b8e2c3a13efdc3
Vladseme2012 <vlad.seme2012@yandex.ru>
2022-05-08 17:57:02 +0300

    added css padding and background-color properties to the ListItem component to change the margins of list items and set the background color, and optional padding and bgColor properties in the IListItemProps interface for the ListItem component

--- sushi-site/src/components/common-components/ListItem/ListItem.tsx
+++ sushi-site/src/components/common-components/ListItem/ListItem.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { themes } from "../../../constants/themes";
 
 interface IListItemProps {
-    margin?: string;
+  margin?: string;
+  padding?: string;
+  bgColor?: string;
 }
 
 export const ListItem = styled.li<IListItemProps>`
   margin: ${p => p.margin};
+  padding: ${p => p.padding || "0"}px;
+  background-color: ${p => p.bgColor || themes.colors.white};
 `

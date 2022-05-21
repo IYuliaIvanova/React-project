0a784be8f3730150526a9d68d4279cb326c3e229
Vladseme2012 <vlad.seme2012@yandex.ru>
2022-05-21 11:00:51 +0300

    commented out the import of the ReviewsPage component

--- sushi-site/src/App.tsx
+++ sushi-site/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Layout } from "./containers/Layout/Layout";
 import { MainPage } from './pages/main-page/MainPage';
+// import { ReviewsPage } from "./pages/reviews-page/ReviewsPage"
 import { OrderingPage } from './pages/ordering-page/OrderingPage';
 import { SushiSetsPage } from "./pages/sushi-sets-page/SushiSetsPage";
 import { ProductDescriptionPage } from './pages/product-description-page/ProductDescriptionPage';
@@ -11,7 +12,7 @@ function App() {
     <Routes>
         <Route element={<Layout/>}>
             <Route path="/" element={<Navigate replace to="/mainPage" />} />
-            <Route path="/mainPage/*" element={<MainPage/>}/>
+            <Route path="/mainPage/*" element={<MainPage />}/>
             <Route path="/sushiSets" element={<SushiSetsPage/>}/>
             <Route path="/sushiSets/:linkTo" element={<ProductDescriptionPage/>}/>
         </Route>
